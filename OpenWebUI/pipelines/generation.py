9a704bba4b906e8f5a550daa4bfad07074e3c665
MagaDaur <magarulit2014@gmail.com>
2024-11-08 21:27:13 +0200

    123

--- OpenWebUI/pipelines/generation.py
+++ OpenWebUI/pipelines/generation.py
@@ -5,29 +5,23 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-client = MilvusClient(uri="http://milvus-standalone:19530", token='root:Milvus')
-client.create_collection('embeddings', 1024)
-
 class Pipeline:
     class Valves(BaseModel):
         pass
 
     def __init__(self):
-        self.name = "Generation"
+        self.name = "Generation" 
 
     async def on_startup(self):
-        # This function is called when the server is started.
-        print(f"on_startup:{__name__}")
-        pass
+        self.milvus_client = MilvusClient(uri="http://milvus-standalone:19530", token='root:Milvus')
+
+        if 'embeddings' not in self.milvus_client.list_collections():
+            self.milvus_client.create_collection('embeddings', 1024)
 
     async def on_shutdown(self):
-        # This function is called when the server is stopped.
-        print(f"on_shutdown:{__name__}")
-        pass
+        self.milvus_client.close()
 
     def pipe(
         self, user_message: str, model_id: str, messages: List[dict], body: dict
     ) -> Union[str, Generator, Iterator]:
-        
-
-        return ''.join(client.list_collections())
+        return f'{user_message}, {model_id}'
