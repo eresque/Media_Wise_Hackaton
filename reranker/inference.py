eaa5e94dd55df81b0cb7fc75f6327423751c8ef2
Gordeev.AI <tema.gor17@gmail.com>
2024-11-09 03:35:23 +0300

    Async reranker micro service

---
+++ reranker/inference.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from sentence_transformers import CrossEncoder
+import asyncio
+
+app = FastAPI()
+
+RERANKER_MODEL = CrossEncoder('DiTy/cross-encoder-russian-msmarco', max_length=512, device='cuda')
+
+class RerankRequest(BaseModel):
+    query: str
+    documents: list[str]
+    top_k: int = 3
+
+def rerank_documents(query: str, documents: list, top_k: int=3):
+    pairs = [[query, doc] for doc in documents]
+    scores = RERANKER_MODEL.predict(pairs)
+    scored_documents = list(zip(scores, documents))
+    ranked_documents = sorted(scored_documents, key=lambda x: x[0], reverse=True)
+    top_k = min(top_k, len(ranked_documents))
+    top_results = ranked_documents[:top_k]
+
+    return top_results
+
+async def async_rerank_documents(query, documents, top_k):
+    return await asyncio.to_thread(rerank_documents, query, documents, top_k)
+
+@app.post("/rerank/")
+async def rerank(request: RerankRequest):
+    top_results = await async_rerank_documents(request.query, request.documents, request.top_k)
+    return {"ranked_documents": top_results}
