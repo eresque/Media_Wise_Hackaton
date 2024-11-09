3839f07e21da76794593820e1b0eb3a11dc28218
MagaDaur <magarulit2014@gmail.com>
2024-11-09 22:36:50 +0200

    123

--- OpenWebUI/pipelines/generation.py
+++ OpenWebUI/pipelines/generation.py
@@ -6,6 +6,7 @@ from pymilvus.client.abstract import SearchResult
 from dotenv import load_dotenv
 import requests
 import json
+import pymupdf
 
 load_dotenv()
 
@@ -37,7 +38,7 @@ class Pipeline:
 
         search_params = {
             "metric_type": "COSINE",
-            "params": {"ef": 64}  # Size of the dynamic candidate list during search
+            "params": {"ef": 64}
         }
 
         collection.load()
@@ -46,31 +47,39 @@ class Pipeline:
             data=vector,
             anns_field='vector',
             param=search_params,
-            limit=10, #TDB
+            limit=10,
             output_fields=['id', 'page_num', 'text', 'orig_file']
         )
 
         documents = []
+        metadata = []
 
         for hits in res:
             for hit in hits:
                 documents.append(hit.entity.get('text'))
-
-
+                metadata.append({
+                    'page': hit.entity.get('page_num'),
+                    'orig': hit.entity.get('orig_file'),
+                    'id': hit.entity.get('id'),
+                })
 
         collection.release()
 
-        yield json.dumps({
-            'query': user_message,
-            'documents': documents,
-        }, ensure_ascii=False)
-
-        response = requests.post('http://reranker_inference:8081/rerank', json={
+        rerank_response = requests.post('http://reranker_inference:8081/rerank', json={
             'query': user_message,
             'documents': documents,
         }).json()
 
-        yield json.dumps(response, ensure_ascii=False)
+        top_metadata = [metadata[documents.index(doc_data[1])] for doc_data in rerank_response['ranked_documents']]
 
+        top_file_name = top_metadata[0]['orig'].split('/')[-1].split('_')[1].split('.')[0]
 
+        yield f'File: {top_file_name}\nPage: {top_metadata['page']}'
+
+        llm_response = requests.post('http://llm_inference:8087/llm-response', json={
+            'prompt': user_message,
+            'context': ' '.join([doc_data[1] for doc_data in rerank_response['ranked_documents']])
+        }).json()
+
+        yield llm_response['response']
 
